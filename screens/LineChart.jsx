654baacf709590ee76e74c6c92d0c07b9429ff4d
atalo80 <atalo80@walla.com>
2022-08-09 21:25:37 +0300

    upStyle

--- screens/LineChart.jsx
+++ screens/LineChart.jsx
@@ -1,43 +1,24 @@
-import React from "react"; 
- 
-import { 
-    StyleSheet, 
-} from 'react-native';
- 
-import { VictoryLine, VictoryChart, VictoryTheme,   } from 'victory-native';
+import React from "react";
 
-const LineChart = props => {   
-  const  data = props.data  ;   
- 
-   return (
- 
-<VictoryChart
-  theme={VictoryTheme.material}
-> 
-        <VictoryLine
+import { StyleSheet } from "react-native";
+
+import { VictoryLine, VictoryChart, VictoryTheme } from "victory-native";
+
+const LineChart = (props) => {
+  const data = props.data;
+
+  return (
+    <VictoryChart theme={VictoryTheme.material}>
+      <VictoryLine
         style={{
           data: { stroke: "#c43a31" },
-          parent: { border: "1px solid #ccc"}
+          parent: { border: "1px solid #ccc" },
         }}
         data={data}
       />
+    </VictoryChart>
+  );
+};
  
-</VictoryChart>
-  )
-  }
- 
-const style = StyleSheet.create({
-  shadow: {
-      shadowColor: "#000",
-      shadowOffset: {
-          width: 2,
-          height: 2,
-      },
-      shadowOpacity: 0.25,
-      shadowRadius: 3.84,
-      elevation: 3,
-  }
-})
-
 
 export default LineChart;
